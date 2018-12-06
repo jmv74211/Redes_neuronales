21d5a79141ebff9ed5878325fa7552f46edd92f7
Jonathan <jmv74211@gmail.com>
2018-12-06 16:53:22 +0100

    Añade fichero para crear gráficos a partir de los datos obtenidos

---
+++ plot_result.py
@@ -0,0 +1,40 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+num_epocs = 6
+
+file='./results/perceptron_multilayer_' + repr(6) + 'e.txt'
+
+epocas = np.loadtxt(file, delimiter='\t', skiprows=0,usecols=[0])
+loss= np.loadtxt(file, delimiter='\t', skiprows=0,usecols=[1])
+acc= np.loadtxt(file, delimiter='\t', skiprows=0,usecols=[2])
+
+plt.figure() 
+plt.plot(epocas,acc)
+plt.title("Variación de acc respecto a épocas")
+plt.xlabel("Acc")
+plt.ylabel("Número de épocas")
+
+
+manager = plt.get_current_fig_manager() 
+manager.resize(*manager.window.maxsize()) 
+
+plt.savefig('./results/multilayer_perceptron_' + repr(num_epocs) + 'e_acc.png')
+
+plt.figure() 
+plt.plot(epocas,loss)
+plt.title("Variación de loss respecto a épocas")
+plt.xlabel("Loss")
+plt.ylabel("Número de épocas")
+
+
+manager = plt.get_current_fig_manager() 
+manager.resize(*manager.window.maxsize()) 
+
+plt.savefig('./results/multilayer_perceptron_' + repr(num_epocs) + 'e_loss.png')
+
+
+
+
+
+
